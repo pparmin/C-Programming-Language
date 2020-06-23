46436e1e8eac921ad3ec9fa308f0379965bcf55b
Philipp Armingeon <philipp.armingeon@googlemail.com>
2020-06-23 12:26:44 +0200

    Updated exercise 23 + .gitignore

--- chapter-1/ex1-23.c
+++ chapter-1/ex1-23.c
@@ -5,7 +5,7 @@
 #define IN_SL_COMMENT 2
 #define IN_ML_COMMENT 3
 #define END_ML_COMMENT 4
-#define IN_COMMENT 5
+
 
 int main()
 {
@@ -40,6 +40,7 @@ int main()
                 state = PROGRAM;
             }
         }
+
         else if (state == IN_ML_COMMENT) {
             if (current_ch == '*') {
                 state = END_ML_COMMENT;
@@ -53,86 +54,6 @@ int main()
                 state = IN_ML_COMMENT;
             }
         }
-
-/*        else if (state == IN_ML_COMMENT) {
-            if (current_ch == '*') {
-                state = END_ML_COMMENT;
-            }
-        }
-
-        else if (state == END_ML_COMMENT) {
-            if (current_ch == '/') {
-                state = PROGRAM;
-            } else {
-                state = IN_ML_COMMENT;
-            }
-        }*/
     }
     return 0;
 }
-
-
-
-/*
-int main()
-{
-  int current_ch, last_ch;
-  int state;
-
-  state = PROGRAM;
-
-  while ((current_ch = getchar()) != EOF) {
-    if (state == PROGRAM) {
-      if (current_ch == '/') {
-        state = BEGIN_COMMENT;
-      }
-      else if (current_ch == 'n' && last_ch == '\\') {
-          putchar(last_ch);
-          putchar(current_ch);
-      }
-      else if (last_ch == '\\' && current_ch != 'n') {
-          state = IN_COMMENT;
-      }
-      else {
-        putchar(current_ch);
-      }
-    }
-    else if (state == BEGIN_COMMENT) {
-        if (current_ch == '/' || current_ch == '*') {
-            state = IN_COMMENT;
-        }
-        else {
-            putchar('/');
-            state = PROGRAM;
-        }
-    }
-    else if (state == IN_COMMENT) {
-        if (current_ch == '*' || current_ch == '/') {
-            state = END_COMMENT;
-        }
-        // Handle line feed (LF or '\n') character
-        // PROBLEM: In multi-line comments this presents an issue: How do I handle multi-line comments
-        // but also handle comments that come after a valid expression (like int len; // length\n int end;)
-
-        else if (current_ch == '\n') {
-            state = PROGRAM;
-        }
-
-    }
-    else if (state == END_COMMENT) {
-        if (current_ch == '/') {
-            state = PROGRAM;
-        }
-        */
-/*else if (current_ch == '\\') {
-            last_ch = current_ch;
-            state = PROGRAM;
-        }*//*
-
-        else {
-            state = IN_COMMENT;
-        }
-    }
-  }
-  return 0;
-}*/
